53c3702686e24ee7aaf26ce84a47383abfcea388
Luca Bacci <luca.bacci982@gmail.com>
2025-06-16 15:13:44 +0200

    dwrite-extra.hpp: Fix check for mingw-w64 workaround

--- src/win32/dwrite-extra.hpp
+++ src/win32/dwrite-extra.hpp
@@ -1,10 +1,5 @@
 /* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
-/* MinGW workarounds
- *
- * It doesn't define operators for DWRITE_GLYPH_IMAGE_FORMATS.
- */
-
 #ifndef DWRITE_EXTRA_H
 #define DWRITE_EXTRA_H
 
@@ -20,7 +15,7 @@ struct DWRITE_COLOR_GLYPH_RUN1_WORKAROUND : DWRITE_COLOR_GLYPH_RUN
 using DWRITE_COLOR_GLYPH_RUN1_WORKAROUND = DWRITE_COLOR_GLYPH_RUN1;
 #endif
 
-#if !defined(__MINGW64_VERSION_MAJOR) || __MINGW64_VERSION_MAJOR < 11
+#if defined(__MINGW64_VERSION_MAJOR) && __MINGW64_VERSION_MAJOR < 11
 DEFINE_ENUM_FLAG_OPERATORS(DWRITE_GLYPH_IMAGE_FORMATS);
 #endif
 
